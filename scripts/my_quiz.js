f70c93fba7131ebc6b4d05d4b7735b8b53306f80
marylaa <maryla.sylwestrzak@gmail.com>
2023-03-21 21:28:20 +0100

    everything works

--- scripts/my_quiz.js
+++ scripts/my_quiz.js
@@ -1,6 +1,9 @@
 const allQuestions = ["2 + 2 = ?", "2 - 10 = ?", "20 * 10 = ?"];
 const allAnswears = [["4", "3", "2"], ["5", "-8", "-2"], ["200", "1000", "500"]];
+const answers = ['a', 'b', 'a'];
 const finish = "The End";
+
+const userAnswears = [];
 
 let intervalId;
 var currentQuestionNumber = 0;
@@ -9,9 +12,7 @@ const updateQuestionTime = 1200;
 let currentQuestion = '';
 let numberOfQuestions = 0;
 
-setUpQuestionDisplay(allQuestions, allAnswears);
-
-function setUpQuestionDisplay(allQuestions, allAnswears) {
+function onLoad() {
     numberOfQuestions = allQuestions.length;
     document.getElementById("question").innerText = allQuestions[0];
     document.getElementById("answear1-label").innerText = allAnswears[0][0];
@@ -43,6 +44,7 @@ function fadeout() {
 }
 
 function updateHtml() {
+    getAnswears()
     document.getElementById("question").innerText = currentQuestion;
     document.getElementById("answear1-label").innerText = allAnswears[currentQuestionNumber][0];
     document.getElementById("answear2-label").innerText = allAnswears[currentQuestionNumber][1];
@@ -51,18 +53,37 @@ function updateHtml() {
 }
 
 function closeQuiz() {
+    getAnswears()
     document.getElementById("question").innerText = finish;
-    document.getElementById("answear1-label").style.display = "none";
-    document.getElementById("answear1").style.display = "none";
+    document.getElementById("user-answears").style.display = "none";
 
     const redirect = document.createElement("a");
     redirect.href="statistics.html";
     redirect.innerText="Check, how you did";
 
-    document.getElementById("answear2-label").innerText = "";
-    document.getElementById("answear2-label").appendChild(redirect)
-    document.getElementById("answear2").style.display = "none";
-    document.getElementById("answear3-label").style.display = "none";
-    document.getElementById("answear3").style.display = "none";
+    document.getElementById("quiz-form").appendChild(redirect);
     document.getElementById("quiz").style.opacity = '1';
+    checkAnswears();
+}
+
+function getAnswears() {
+    const form = document.getElementById("quiz-form");
+    const answer = form["answear"].value;
+    const radios = document.getElementsByTagName("input");
+    radios[0].checked = false;
+    radios[1].checked = false;
+    radios[2].checked = false;
+
+    userAnswears.push(answer);
+}
+
+function checkAnswears() {
+    for (let i = 0; i < allAnswears.length; i++) {
+        if (userAnswears[i] === answers[i]) {
+            document.cookie = (String)(i + 1) + "=correct";
+        } else {
+            document.cookie = (String)(i + 1) + "=incorrect";
+        }
+    }
+    console.log(document.cookie);
 }
--- scripts/statistics.js
+++ scripts/statistics.js
@@ -1,4 +1,5 @@
 function onLoad() {
     document.getElementById("answer1").innerHTML = document.cookie.split(";")[0].split("=")[1];
     document.getElementById("answer2").innerHTML = document.cookie.split(";")[1].split("=")[1];
+    document.getElementById("answer3").innerHTML = document.cookie.split(";")[2].split("=")[1];
 }
